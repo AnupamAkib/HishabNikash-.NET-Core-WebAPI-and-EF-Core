5fe631aaa40f9fa66fe7f02853c86c97777c1cf7
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2024-09-18 00:24:02 +0600

    Hishab repository functionality added

--- Repositories/IHishabRepository.cs
+++ Repositories/IHishabRepository.cs
@@ -5,11 +5,11 @@ namespace HishabNikash.Repositories
     public interface IHishabRepository
     {
         Task<Hishab> CreateNewHishabAsync(Hishab hishab);
-        Task<List<Hishab>> GetHishabsByUserAsync(int userId);
+        Task<List<Hishab>> GetHishabsByUserAsync(int userID);
         Task<Hishab> GetHishabByIDAsync(int hishabID);
-        Task<Hishab> IncreaseAmountAsync(int hishabID, int amount);
-        Task<Hishab> DecreaseAmountAsync(int hishabID, int amount);
-        Task<Hishab> EditHishabAsync(int hishabID, string updatedHishabName, string updatedCardColor);
+        Task<Hishab?> IncreaseAmountAsync(int hishabID, int amount);
+        Task<Hishab?> DecreaseAmountAsync(int hishabID, int amount);
+        Task<Hishab?> EditHishabAsync(int hishabID, string updatedHishabName, string updatedCardColor);
         Task<bool> DeleteHishabAsync(int hishabID);
     }
 }
