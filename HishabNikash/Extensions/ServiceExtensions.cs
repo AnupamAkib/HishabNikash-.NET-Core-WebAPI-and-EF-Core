cae4c63493d773607ca32c0ef2e442edd69eb42d
Mir Anupam Hossain Akib <anupam.akib@welldev.io>
2024-10-07 09:53:14 +0600

    3.7 Creating Repository Manager

---
+++ HishabNikash/Extensions/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Contracts;
+using Repository;
+
+namespace HishabNikash.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+    }
+}
