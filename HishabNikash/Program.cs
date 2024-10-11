4d449d04dce89d6723ac9b679aca6aec5556d07e
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2024-10-11 22:22:54 +0600

    3.9 Register RepositoryContext at a Runtime

--- HishabNikash/Program.cs
+++ HishabNikash/Program.cs
@@ -7,11 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<RepositoryContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
-
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
